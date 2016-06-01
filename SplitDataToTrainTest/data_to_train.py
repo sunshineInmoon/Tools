dd8be338340cc5b3a97c92bda61de324a43a6012
sunshineInmoon <451413025@qq.com>
2016-06-01 10:39:04 +0800

    dataset to triandata

--- SplitDataToTrainTest/data_to_train.py
+++ SplitDataToTrainTest/data_to_train.py
@@ -16,10 +16,11 @@ import SplitDataTo_Train_Test
 输入参数：dir_path----预处理后的数据，一般是保存文件的问价夹
          out_path----处理后保存文件夹和文件的路径
          Format----转换后的数据格式默认leveldb
+         command----命令行
 '''
-def DataToTrain(dir_path,out_path,Format='leveldb',parencent=0.1):
+def DataToTrain(dir_path,out_path,command,Format='leveldb',parencent=0.1):
     #第一步，分离数据
-   print u'第一步，分离数据'
+   print u'第一步，分离数据----开始'
    train_data = out_path + '/' + 'train_data'
    if not os.path.exists(train_data):
        os.mkdir(train_data)
@@ -27,11 +28,13 @@ def DataToTrain(dir_path,out_path,Format='leveldb',parencent=0.1):
    if not os.path.exists(test_data):
        os.mkdir(test_data)
    SplitDataTo_Train_Test.SplitData(dir_path,train_data,test_data,parencent)
+   print u'第一步，分离数据----结束。'
    #第二步，转换数据格式
+   print u'第二步，转换数据格式----开始'
    train_imagelist = out_path + '/' + 'train_imagelist.txt'
    text_process.CreatImageListWithLabel(train_data,train_imagelist)
    train = out_path + '/' + 'train_' + Format
-   command_train = "%s %s%s %s %s %s"%('convert_imageset.exe','--backend=',Format,train_data,\
+   command_train = "%s %s %s %s %s"%('convert_imageset.exe',command,train_data,\
    train_imagelist,train)
    print command_train
    os.system(command_train)
@@ -39,12 +42,13 @@ def DataToTrain(dir_path,out_path,Format='leveldb',parencent=0.1):
    test_imagelist = out_path + '/' + 'test_imagelist.txt'
    text_process.CreatImageListWithLabel(test_data,test_imagelist)
    test = out_path + '/' + 'test_' + Format
-   command_test = "%s %s%s %s %s %s"%('convert_imageset.exe','--backend=',Format,test_data,\
+   command_test = "%s %s %s %s %s"%('convert_imageset.exe',command,test_data,\
    test_imagelist,test)
    print command_test
    os.system(command_test)
-   
+   print u'第二步，转换数据格式----结束'
    #第三步，计算平均值
+   print u'第三步，计算平均值----开始'
    train_mean = out_path + '/' + 'train_mean.binaryproto'
    command_train_mean = "%s %s%s %s %s"%('compute_image_mean.exe','--backend=',Format,train,train_mean)
    print command_train_mean
@@ -53,8 +57,10 @@ def DataToTrain(dir_path,out_path,Format='leveldb',parencent=0.1):
    test_mean = out_path + '/' + 'test_mean.binaryproto'
    command_test_mean = "%s %s%s %s %s"%('compute_image_mean.exe','--backend=',Format,test,test_mean)
    os.system(command_test_mean)
-   
+   print u'第三步，计算平均值----结束'
 if __name__ == '__main__':
     dir_path = 'E:/practical_face/LogPhoto'
     out_path = 'E:/practical_face'
-    DataToTrain(dir_path,out_path)
+    command = '--backend=leveldb --gray=true --shuffle=ture'
+    command = '--backend=leveldb --gray=true --shuffle=ture --resize_height=144 --resize_width=144'
+    DataToTrain(dir_path,out_path,command)
